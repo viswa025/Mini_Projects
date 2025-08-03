357b9d93510786e13c44a296df84c659e873b128
viswa027 <viswa2k03@gmail.com>
2025-08-03 14:45:10 +0530

    basic model

--- slot_machine.java
+++ slot_machine.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 public class slot_machine{
     
@@ -9,6 +10,10 @@ public class slot_machine{
         String[] spin={"💵","🍒","🌟","🔔"};
         Random rand=new Random();
         boolean again=true;
+        int add;
+        String test;
+        int spend=0;
+        int profit=0;
 
 
 
@@ -22,42 +27,81 @@ public class slot_machine{
         scan.nextLine();
         if(topup>0) {
             balance+=topup;
-            System.out.println("Your Current balance is:₹"+balance);
+            spend = spend + topup;
+
             break;}
         else{
             System.out.println("Invalid");
         }}while(true);
         do{
+            System.out.println("Your Current balance is:₹"+balance);
          System.out.print("Enter Your bet amount:₹");
          bet=scan.nextInt();
          scan.nextLine();
-        if(bet<balance) {
+        if(bet<=balance && bet>0 && balance>0) {
             balance-=bet;
+
             System.out.println("Your Current balance is:₹"+balance);
-            break;}
+            }
         else{
             System.out.println("Invalid");
-        }}while(true);
+            System.out.println("Your Current balance is:₹"+balance);
+            System.out.println("Do you want to add balance(Y/n):");
+            test= scan.nextLine().toLowerCase();
+            if (test.equals("n") ){
+
+                break;
+
+            }
+            else if (test.equals("y")){
+            System.out.print("How much amount do you want to topup:₹");
+            add= scan.nextInt();
+            scan.nextLine();
+            if(add>0){balance+=add; spend+=topup;}}
+            else {
+                System.out.print("Invalid");
+            }
+
+            continue;
+        }
 
         System.out.println("Spinning.....");
-        do { 
+
             int i=rand.nextInt(4);
             int j=rand.nextInt(4);
             int k=rand.nextInt(4);
             System.out.print(spin[i]+" "+spin[j]+" "+spin[k]+" \n");
 
+            if(i==j && j==k) {
+                balance += bet * 5;
+                System.out.println("You have won:₹" + (bet * 5));
+            }
+            else if(i==j || j==k || k==i){
+                balance+=bet*2;
+                System.out.println("You have won:₹"+(bet*2));}
+            else System.out.println("You lost the game");
 
 
-            System.out.print("Do you wish to continue(Yes/No)");
+
+            System.out.println("Your current balance:₹"+balance);
+
+
+            System.out.print("Do you wish to continue(Y/N):");
             String Checkcontinue=scan.nextLine().toLowerCase();
-            if (Checkcontinue.equals("no") ){
-                System.out.println("Thank you");
+            if (Checkcontinue.equals("n") ){
+
                 again=false;
 
             }
 
             
         } while (again);
+        System.out.println("You have spended:₹"+spend);
+        if(balance>=spend) System.out.println("You have got:₹"+(balance-spend));
+        else System.out.println("You have lose:₹"+(spend-balance));
+        System.out.println("************************************");
+        System.out.println("      Thank You,Visit again         ");
+        System.out.println("************************************");
 
         
         
