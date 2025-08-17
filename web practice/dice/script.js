aa7a9cfc5ad867ec5a64802ee46837a3fcb529b9
viswa027 <viswa2k03@gmail.com>
2025-08-17 19:22:58 +0530

    project-03

---
+++ web practice/dice/script.js
@@ -0,0 +1,16 @@
+
+
+function roll(){
+    const number=document.getElementById("number").value;
+    const diceresult=document.getElementById("diceresult");
+    const diceresult1=document.getElementById("diceresult1");
+    let result=[];
+    let image=[];
+    for(let i=0;i<number;i++){
+        let n=Math.floor(Math.random()*6)+1;
+
+    }
+
+
+
+}
